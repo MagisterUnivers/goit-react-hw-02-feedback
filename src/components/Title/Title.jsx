370116845d6d16612c8bb57147427e3114e19c02
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-04-22 02:10:26 +0300

    Update

--- src/components/Title/Title.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-import css from './Title.module.css';
-const Title = ({ text }) => <h2 className={css.sectionTitle}>{text}</h2>;
-
-export default Title;
-
-Title.propTypes = {
-  text: PropTypes.string.isRequired,
-};
